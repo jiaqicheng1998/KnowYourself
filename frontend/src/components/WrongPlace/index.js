1a4ea8521b3adae9ef034754feeabdd20c989001
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-08-11 15:33:48 -0700

    wrong page

--- frontend/src/components/WrongPlace/index.js
+++ frontend/src/components/WrongPlace/index.js
@@ -1,7 +1,15 @@
+import React from "react";
+import './WrongPlace.css'
+
+
 const WrongPlace = () => {
     return (
-        <h1>Opps! you are not supposed to be here!
-            Please login and try again!</h1>
+        <div className="wrong_page">
+            <div className="wrong_pic" style={{ backgroundImage: `url(https://cdn.dribbble.com/users/2155753/screenshots/6716126/error_prancheta_1_co_pia.png)` }}></div>
+            <p>Oops! you are not supposed to be here!
+                Please login and try again!
+            </p>
+        </div>
     )
 }
 
