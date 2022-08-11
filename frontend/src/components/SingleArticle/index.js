f5b0a6910a0b0911a339935a42f3b141bb4ea3be
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-08-11 14:20:36 -0700

    pregrade gogogo

--- frontend/src/components/SingleArticle/index.js
+++ frontend/src/components/SingleArticle/index.js
@@ -30,62 +30,78 @@ const SingleArticle = () => {
     }
 
     return (
-        <div className='sigle-article-page'>
-            <div className='title-and-button'>
-                <h1>{article?.title}</h1>
-                {user?.id === article?.user_id && (
-                    <div className='edit-and-del-article'>
-                        <div className="edit-article">
-                            <NavLink to={`/articles/${articleId}/edit`}>
-                                <i className="fa-solid fa-pen-to-square fa-xl" />
-                            </NavLink>
+        <div>
+            <div className='sigle-article-page'>
+                <div className='title-and-button'>
+                    <h1>{article?.title}</h1>
+                    {user?.id === article?.user_id && (
+                        <div className='edit-and-del-article'>
+                            <div className="edit-article">
+                                <NavLink to={`/articles/${articleId}/edit`}>
+                                    <i className="fa-solid fa-pen-to-square fa-xl" />
+                                </NavLink>
+                            </div>
+                            <div className="edit-article">
+                                <span onClick={handleClick}>
+                                    <i className="fa-solid fa-trash-can fa-xl" />
+                                </span>
+                            </div>
                         </div>
-                        <div className="edit-article">
-                            <span onClick={handleClick}>
-                                <i className="fa-solid fa-trash-can fa-xl" />
-                            </span>
-                        </div>
+                    )}
+                </div>
+                <div className="name-and-profile-article">
+                    <div className="user-icon" style={{ backgroundImage: `url(${article?.User?.profile_pic})` }}></div>
+                    <p>{article?.User?.username}</p>
+                </div>
+                <div className="banner-article" style={{ backgroundImage: `url(${article?.img_url})` }}></div>
+                {article?.content && (
+                    <p>{parse(article?.content)}</p>
+                )}
+                {selectedComments.length != 0 && (
+                    <div className='comments-container'>
+                        <h1>Comments:</h1>
+                        {Object.values(selectedComments).map((comment) => (
+                            <div className='ind-comment' key={comment?.id}>
+                                <div className='ind-comment-top'>
+                                    <div className="name-and-profile">
+                                        <div className="user-icon" style={{ backgroundImage: `url(${comment?.User.profile_pic})` }}></div>
+                                        <p>{comment.User?.username || user.username}</p>
+                                    </div>
+                                    <div>
+                                        {user?.id === comment?.user_id && (
+                                            <div className='edit-and-del-article'>
+                                                <div className="edit-article">
+                                                    <NavLink to={`/articles/${articleId}/comments/${comment.id}/edit`}>
+                                                        <i className="fa-solid fa-pen-to-square" />
+                                                    </NavLink>
+                                                </div>
+                                                <div className="edit-article">
+                                                    <span
+                                                        onClick={
+                                                            async (e) => {
+                                                                e.preventDefault();
+                                                                await dispatch(deleteCommentThunk(comment.id))
+                                                                    .then(() => dispatch(loadCommentsThunk()))
+                                                            }
+                                                        }
+                                                    >
+                                                        <i className="fa-solid fa-trash-can" />
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                                <p className='comment-content'>{comment?.content}</p>
+                            </div>
+                        ))}
                     </div>
                 )}
             </div>
-            <div className="name-and-profile-article">
-                <div className="user-icon" style={{ backgroundImage: `url(${article?.User?.profile_pic})` }}></div>
-                <p>{article?.User?.username}</p>
-            </div>
-            <div className="banner-article" style={{ backgroundImage: `url(${article?.img_url})` }}></div>
-            {article?.content && (
-                <p>{parse(article?.content)}</p>
-            )}
-            <div>
+            <div id='write-comment'>
                 <NavLink to={`/articles/${articleId}/comments/new`}>
-                    <button>Add comment</button>
+                    <i className="fa-solid fa-comment-dots fa-2x"></i>
                 </NavLink>
-            </div>
-            <div>
-                {Object.values(selectedComments).map((comment) => (
-                    <div className='ind-comment' key={comment?.id}>
-                        <p>{comment.User?.username || user.username}</p>
-                        <p>{comment?.content}</p>
-                        {user?.id === comment?.user_id && (
-                            <div>
-                                <NavLink to={`/articles/${articleId}/comments/${comment.id}/edit`}>
-                                    <i className="fa-solid fa-pen-to-square" />
-                                </NavLink>
-                                <span
-                                    onClick={
-                                        async (e) => {
-                                            e.preventDefault();
-                                            await dispatch(deleteCommentThunk(comment.id))
-                                                .then(() => dispatch(loadCommentsThunk()))
-                                        }
-                                    }
-                                >
-                                    <i className="fa-solid fa-trash-can" />
-                                </span>
-                            </div>
-                        )}
-                    </div>
-                ))}
             </div>
         </div>
     )
