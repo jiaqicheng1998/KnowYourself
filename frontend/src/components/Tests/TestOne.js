53a48b2f85e96ea776b887a07dd4cadf9199f55a
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-09-13 14:46:56 -0700

    changed test one

--- frontend/src/components/Tests/TestOne.js
+++ frontend/src/components/Tests/TestOne.js
@@ -30,28 +30,28 @@ const TestOne = ({ isLoaded }) => {
             result = 'You go with the flow. You prefer happiness and comfort in your mind rather than abundance in material world. You are not very revolutional. You do not like it when your peaceful day-to-day life are interrupted. Same mindset applies to your way of chasing your dream.'
         }
         if (option == 5) {
-            result = 'you are often weary and feel weak, and you have little energy for life.'
+            result = 'You are often weary and feel weak, and you have little energy for life. There are very little things can attract you in life. You tend to keep your sorrow and annoyance in your mind. Your do not like to talk to others about it or find other ways to release the stress.'
         }
         if (option == 9) {
-            result = 'you are a merry and fun-loving person.'
+            result = 'You are a merry and fun-loving person. You are a vibe-maker in your community. You do not care that much about details, and you are a candid person. You enjoy hanging out with others in a rather easier way. You are full of curiosity. Once you find your interest, you will make something out of it.'
         }
         if ([13, 21].includes(parseInt(option))) {
-            result = 'you have got the tendency to keep to yourself, you are also frequently anxious and would normally avoid other people.'
+            result = 'You have got the tendency to keep to yourself, you are also frequently anxious and would normally avoid other people. However, you will wear your friendly social mask on. It is very hard to walk in your heart. Aviodance is a usual move for you. You cover yourself too well. You do not want to share secrets with others and you are not good at communication.'
         }
         if (option == 8) {
-            result = `you are a real daydreamer — you like being in your own world and away from this one.`
+            result = `You are a real daydreamer — you like being in your own world and away from this one. You are kind and naive. You have high self-esteem and you are proud of yourself. You go with your own inituition. You would like to fantasize, and do not care the judgements from outside world. If you insist on something, it is hard to talk you out of it.`
         }
         if (option == 10 || option == 15) {
-            result = 'you are a well-adapted person, content with the life you have.'
+            result = 'You are a well-adapted person, content with the life you have. You refuse to be vulgar and advocate noble. You have your own standards to the quality of your life. You are innocent. You do not want to be aggressive or competitive most of the time. However, when you feel like you deserve to have it, you will put in all your efforts.'
         }
         if (option == 14) {
-            result = 'you are emotionally drained or exhausted. There is a good chance you are facing inner emotional crisis or turmoil.'
+            result = 'You are emotionally drained or exhausted. There is a good chance you are facing inner emotional crisis or turmoil. No matter in relationships or life, you hope for a turnover in the outside enviornment. However, you resist to make any changes on yourself.'
         }
         if (option == 20) {
-            result = 'You have heightened sense of self-importance. You are a born leader, and you want to be the only one people listen to.'
+            result = 'You have heightened sense of self-importance. You are a born leader, and you want to be the only one people listen to. However, you will hurt the feelings of people who really care about you sometime because of your pride and ignorance.'
         }
         if (option == 16) {
-            result = "You are likely to lean on other people. When facing obstacles, you always wish to get help and support from your surroundings. You looks like you are careless, but in reality, you are very sensitive."
+            result = "You are likely to lean on other people. When facing obstacles, you always wish to get help and support from your surroundings. You looks like you are careless, but in reality, you are very sensitive. You need to find your own strength and try to be independent."
         }
         if (option == 17) {
             result = "You have a strong sense of responsiblity. You are a trustworthy person. Your friend will definitely feel better after your consolation. However, you feel lonely inside and you are afraid of it. You want people's attention at all time."
