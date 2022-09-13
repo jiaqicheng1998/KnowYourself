f0ea245afcffae14d27e5bd311bd35a81d98c1c6
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-09-13 14:01:58 -0700

    result page css

--- frontend/src/components/Result/index.js
+++ frontend/src/components/Result/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadResultsThunk } from '../../store/result';
 import { useParams } from 'react-router-dom';
+import './Result.css'
 
 const Result = () => {
     const resultId = useParams().id;
@@ -13,9 +14,11 @@ const Result = () => {
     const result = results[resultId]
 
     return (
-        <div>
+        <div className='result-page'>
             <h1>Your Result for {result?.title}</h1>
+            <p>Tested at: {result?.createdAt.slice(0,10)}</p>
             <p>{result?.result}</p>
+            <div className="result-pic" style={{ backgroundImage: `url("https://www.mdanderson.org/images/publications/cancerwise/M/Access_MyChart.jpg")` }}></div>
         </div>
     )
 }
